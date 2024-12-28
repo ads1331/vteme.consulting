a6d895d53cd2ef880ebb107b26dc067af63e8f5a
ads1331 <afanasden21@mail.ru>
2024-12-28 20:48:38 +0400

    add datainlife email in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,8 +12,6 @@ class UserSeeder extends Seeder
         User::create(['name' => 'Иванов', 'email' => 'test@test.ru']);
         User::create(['name' => 'Иванов2', 'email' => 'test2@test.ru']);
         User::create(['name' => 'Иванов3', 'email' => 'test3@test.ru']);
-        User::create(['name' => 'Иванов4', 'email' => 'test4@test.ru']);
-        User::create(['name' => 'Иванов5', 'email' => 'test5@test.ru']);
         User::create(['name' => 'Иванов6', 'email' => 'info@datainlife.ru']);
         User::create(['name' => 'Иванов7', 'email' => ' job@datainlife.ru']);
 
